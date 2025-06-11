0fc85d0a5f87bcef6db2b01fc4fca3855003079f
Joren Frederickx <joren.frederickx@student.ehb.be>
2025-06-11 12:22:55 +0200

    Update hamburger navigatie

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
 
 console.log("hello world")
+
+const nav = document.querySelector('nav');
+navToggle.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
+
+document.querySelectorAll('.dropdown-toggle').forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    toggle.nextElementSibling.classList.toggle('open');
+  });
+});
