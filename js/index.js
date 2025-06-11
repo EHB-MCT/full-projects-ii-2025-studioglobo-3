2e853d475174520ae4b0cb0c6ebb997deafe1cd7
Joren Frederickx <joren.frederickx@student.ehb.be>
2025-06-11 14:55:36 +0200

    Update mappenstructuur

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
-
-console.log("hello world")
-
+// 1. Selecteer de button
+const navToggle = document.querySelector('.nav-toggle');
+// 2. Selecteer de nav-list
 const nav = document.querySelector('nav');
-navToggle.addEventListener('click', () => {
-  nav.classList.toggle('open');
-});
 
-document.querySelectorAll('.dropdown-toggle').forEach(toggle => {
-  toggle.addEventListener('click', () => {
-    toggle.nextElementSibling.classList.toggle('open');
+// 3. Wacht tot de button bestaat (optioneel als script onderaan staat)
+if (navToggle && nav) {
+  navToggle.addEventListener('click', () => {
+    nav.classList.toggle('open');
   });
-});
+  
+  // dropdown op mobile
+  document.querySelectorAll('.dropdown-toggle').forEach(toggle => {
+    toggle.addEventListener('click', () => {
+      toggle.nextElementSibling.classList.toggle('open');
+    });
+  });
+}
